20e553ad74a8c4a70607f2f86d178aa4a8083203
nao20010128nao <nao20010128@gmail.com>
2017-10-26 19:36:21 +0900

    修正

--- src/main/groovy/com/nao20010128nao/DroidComplex/interpreter/ComplexInterpreter.groovy
+++ src/main/groovy/com/nao20010128nao/DroidComplex/interpreter/ComplexInterpreter.groovy
@@ -1,15 +1,20 @@
 package com.nao20010128nao.DroidComplex.interpreter
 
 import android.content.Context
+import dalvik.system.DexClassLoader
+import groovy.transform.CompileStatic
 import me.champeau.groovydroid.GrooidShell
 import org.apfloat.Apcomplex
 import org.apfloat.ApcomplexMath
 import org.apfloat.Apfloat
+import org.codehaus.groovy.control.CompilerConfiguration
 
 import java.util.regex.Pattern
 
 public class ComplexInterpreter {
     static final String REGEX_NUMBER="([0-9]*\\.[0-9]+|[0-9]+)"
+    static final String REGEX_SIGN="([+-])"
+    static final String REGEX_SIGN_OPTIONAL="([+-]?)"
 
     private static ComplexInterpreter instance
     private static boolean metaClassInstalled=false
@@ -18,7 +23,7 @@ public class ComplexInterpreter {
         if(instance){
             return
         }
-        instance=new ComplexInterpreter(app.cacheDir,app.classLoader)
+        instance=new ComplexInterpreter(app?.cacheDir,app?.classLoader)
         installMetaClass()
     }
 
@@ -51,6 +56,9 @@ public class ComplexInterpreter {
         metaClassInstalled=true
     }
 
+    @CompileStatic
+    static ComplexInterpreter getInstance(){instance}
+
     private static Apcomplex convert(a){
         if(a instanceof Apcomplex){
             /* No needs to be converted */
@@ -78,60 +86,81 @@ public class ComplexInterpreter {
              * i[imag], j[imag]
              * [imag]i, [imag]j
              * */
-            def first="[+-]?[ij]"
-            def second="$REGEX_NUMBER[+-][ij]$REGEX_NUMBER"
-            def third="$REGEX_NUMBER[+-]$REGEX_NUMBER[ij]"
-            def forth="[+-]?[ij]$REGEX_NUMBER"
-            def fifth="[+-]?$REGEX_NUMBER[ij]"
+            def prefix="^$REGEX_SIGN_OPTIONAL"
+            def first="$prefix[ij]\$"
+            def secnd="$prefix$REGEX_NUMBER$REGEX_SIGN[ij]$REGEX_NUMBER\$"
+            def third="$prefix$REGEX_NUMBER$REGEX_SIGN$REGEX_NUMBER[ij]\$"
+            def forth="$prefix[ij]$REGEX_NUMBER\$"
+            def fifth="$prefix$REGEX_NUMBER[ij]\$"
             if(inStr.matches(first)){
-                return Apcomplex.I
-            }else if(inStr.matches(second)||inStr.matches(third)){
+                if(inStr.startsWith("-")){
+                    return -Apcomplex.I
+                }else {
+                    return Apcomplex.I
+                }
+            }else if(inStr.matches(secnd)||inStr.matches(third)){
                 def (re,im)=inStr.findAll(Pattern.compile(REGEX_NUMBER)).collect{new Apfloat(it)}
+                if(inStr.startsWith("-")){
+                    re=-re
+                }
+                if(inStr.substring(1).startsWith("-")){
+                    im=-im
+                }
                 return new Apcomplex(re,im)
             }else if(inStr.matches(forth)||inStr.matches(fifth)){
                 def im=inStr.findAll(Pattern.compile(REGEX_NUMBER)).collect{new Apfloat(it)}
-                return new Apcomplex(Apfloat.ZERO,im)
+                def result= new Apcomplex(Apfloat.ZERO,im)
+                if(inStr.startsWith("-")){
+                    return -result
+                }else {
+                    return result
+                }
             }
         }
         throw new NumberFormatException("We can't understand what you request: $a")
     }
 
-    final File cache
-    final GrooidShell compiler
+    Impl impl
 
     private ComplexInterpreter(File cache,ClassLoader loader){
-        this.cache=cache
-        this.compiler=new GrooidShell(cache,loader)
-    }
-
-    public Compilation compile(String script){
-        def result=new Compilation()
-        try{
-            def compileResult=compiler.evaluate(script){cfg->
-                cfg.scriptBaseClass="$DroidComplexScript.name"
-            }
-            result.success=true
-            result.script=compileResult.script
-        }catch (Throwable e){
-            result.success=false
-            result.error=e
+        if(loader instanceof DexClassLoader){
+            impl=new AndroidImpl()
+        }else{
+            impl=new JvmImpl()
         }
-        return result
+        impl.init(cache,loader)
     }
 
+    @CompileStatic
+    public Compilation compile(String script){
+        return impl.compile(script)
+    }
 
+    @CompileStatic
     class Compilation{
         boolean success
-        Script script
+        DroidComplexScript script
         Throwable error
     }
 
+    @CompileStatic
     static abstract class DroidComplexScript extends Script{
         public DroidComplexScript(){
-            super(new ComplexNumberBinding())
+            super(ComplexNumberBinding.instance)
+        }
+
+        @Override
+        Binding getBinding() {
+            return binding=ComplexNumberBinding.instance
+        }
+
+        @Override
+        void setBinding(Binding binding) {
+            super.setBinding(ComplexNumberBinding.instance)
         }
     }
 
+    @Singleton
     static class ComplexNumberBinding extends Binding{
         @Override
         Object getVariable(String name) {
@@ -140,9 +169,9 @@ public class ComplexInterpreter {
             }else if(name.matches("^[ij]$REGEX_NUMBER\$")||name.matches("^$REGEX_NUMBER[ij]\$")){
                 return new Apcomplex(Apfloat.ZERO,new Apfloat(name.findAll(REGEX_NUMBER).first()))
             }else if(name.matches('^[ij]')&&super.getVariable(name) instanceof Number){
-                return super.getVariable(name.substring(1))*Apcomplex.I
+                return convert(super.getVariable(name.substring(1)))*Apcomplex.I
             }else if(name.matches('[ij]$')&&super.getVariable(name) instanceof Number){
-                return super.getVariable(name.substring(0,name.length()-1))*Apcomplex.I
+                return convert(super.getVariable(name.substring(0,name.length()-1)))*Apcomplex.I
             }
             return super.getVariable(name)
         }
@@ -153,6 +182,64 @@ public class ComplexInterpreter {
                 throw new Error('Cannot set to any field starts with "i" or "j".')
             }
             super.setVariable(name, value)
+        }
+    }
+
+    private interface Impl{
+        void init(File cache,ClassLoader loader)
+        Compilation compile(String script)
+    }
+
+    @CompileStatic
+    private class AndroidImpl implements Impl{
+        File cache
+        GrooidShell compiler
+
+        @Override
+        void init(File cache, ClassLoader loader) {
+            this.cache=cache
+            this.compiler=new GrooidShell(cache,loader)
+        }
+
+        @Override
+        Compilation compile(String script) {
+            def result=new Compilation()
+            try{
+                def compileResult=compiler.evaluate(script){CompilerConfiguration cfg->
+                    cfg.scriptBaseClass="$DroidComplexScript.name"
+                }
+                result.success=true
+                result.script=(DroidComplexScript)compileResult.script
+            }catch (Throwable e){
+                result.success=false
+                result.error=e
+            }
+            return result
+        }
+    }
+    @CompileStatic
+    private class JvmImpl implements Impl{
+        final GroovyShell compiler=new GroovyShell(new CompilerConfiguration().with {
+            scriptBaseClass="$DroidComplexScript.name"
+            it
+        })
+
+        @Override
+        void init(File cache, ClassLoader loader) {
+        }
+
+        @Override
+        Compilation compile(String script) {
+            def result=new Compilation()
+            try{
+                def compileResult=compiler.parse(script)
+                result.success=true
+                result.script=(DroidComplexScript)compileResult
+            }catch (Throwable e){
+                result.success=false
+                result.error=e
+            }
+            return result
         }
     }
 }
--- src/test/groovy/com/nao20010128nao/DroidComplex/interpreter/ComplexInterpreterTest.groovy
+++ src/test/groovy/com/nao20010128nao/DroidComplex/interpreter/ComplexInterpreterTest.groovy
@@ -1,20 +1,25 @@
 package com.nao20010128nao.DroidComplex.interpreter
 
+import junit.framework.TestCase
 import org.apfloat.Apcomplex
 import org.apfloat.Apfloat
 
 /**
  * Created by lesmi on 17/10/26.
  */
-class ComplexInterpreterTest extends GroovyTestCase {
+class ComplexInterpreterTest extends TestCase {
+    def interpreter=ComplexInterpreter.instance
     static{
-        ComplexInterpreter.installMetaClass()
+        ComplexInterpreter.install(null)
     }
+    ComplexInterpreter.Compilation compile(String s){
+        interpreter.compile(s)
+    }
+
     void testSimple(){
-        def compiler=new GroovyShell()
-        def parsed=compiler.parse('2+i3')
-        parsed.binding=new ComplexInterpreter.ComplexNumberBinding()
-        Apcomplex result=parsed.run()
+        def script=compile('2+i3')
+        assert script.success
+        Apcomplex result=script.script.run()
         assert result.real()==new Apfloat(2)
         assert result.imag()==new Apfloat(3)
     }
